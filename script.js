c2ec3afffd64f137fa4810b1be39a4d54303167d
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-08-31 04:19:32 +0530

    compute

--- script.js
+++ script.js
@@ -28,7 +28,32 @@ class Calculator {
     this.currentOperand = "";
   }
 
-  compute() {}
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previosOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = "";
+    this.previosOperand = "";
+  }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
@@ -65,4 +90,9 @@ operationButtons.forEach((button) => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
 });
