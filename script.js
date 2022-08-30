7ead6458fdeca866bc52d03ae2367760cdec564e
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-08-30 08:38:04 +0530

    appendNumber

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+class Calculator {
+  constructor(previosOperandTextElement, currentOperandTextElement) {
+    this.previosOperandTextElement = previosOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previosOperand = "";
+    this.operation = undefined;
+  }
+
+  delete() {}
+
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+}
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
@@ -9,3 +35,15 @@ const previosOperandTextElement = document.querySelector(
 const currentOperandTextElement = document.querySelector(
   "[data-current-operand]"
 );
+
+const calculator = new Calculator(
+  previosOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
